236f696a89cd509a1ac72a8f149664b00f7e6b16
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2023-09-07 19:05:44 -0300

    matches adjustments

--- brasileirao/webapp/controller/Main.controller.js
+++ brasileirao/webapp/controller/Main.controller.js
@@ -1,14 +1,28 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,JSONModel) {
         "use strict";
 
         return Controller.extend("brasileirao.controller.Main", {
             onInit: function () {
+                // vamos criar um modelo
+                //antes, as variaveis que vao dentro do modelo
+                var dadosGerais = {
+                    rodada: '89a',
+                    campeonato: "Brasileirão 2023 Fiorinet (lido do Model local)"
+                };
+
+                //aghora sim, vamos criar o modelo
+                var dadosModel = new JSONModel();
+                dadosModel.setData(dadosGerais);
+                var view = this.getView(); //mesma coisa q usar Controller.getView();
+                view.setModel(dadosModel, "ModeloDadosGerais");
+
 
             }
         });

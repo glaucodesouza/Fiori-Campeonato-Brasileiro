2dfc2e62c53ab0ce8be13fa89c4ec87aeb507230
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2023-10-14 22:45:42 -0300

    final da aula video 3

--- brasileirao/webapp/controller/Main.controller.js
+++ brasileirao/webapp/controller/Main.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("brasileirao.controller.Main", {
@@ -45,7 +45,10 @@ sap.ui.define([
                 //atribuimo  3 modelos a tela (view)
                 this.getView().setModel(dadosModel, "ModeloDadosGerais");
                 this.getView().setModel(classificacaoModel, "ModeloClassificacao");
-                this.getView().setModel(dadosModel, "ModeloPartidas");
+                this.getView().setModel(partidasModel, "ModeloPartidas");
+
+                this.buscarDadosGerais();
+                this.buscarClassificacao();
 
 
             },
@@ -65,9 +68,105 @@ sap.ui.define([
                     async: true,
                     crossDomain: true,
                     headers: {
-                        "authorization": "Bearer live_c01acd31e279debe09670e938d2cb1"
+                        "Authorization": "Bearer live_c01acd31e279debe09670e938d2cb1"
                     }
                 };
+
+                //fazemos a chamada da API
+                $.ajax(configuracoes)
+                
+                //sucesso
+                .done(function(resposta) {
+                    // debugger;
+                    dadosModel2.setData(resposta);
+                    this.buscarPartidas(resposta.rodada_atual.rodada);
+                    //IMPORTANTE
+                    //.bind(this) serve para a função atual
+                    // reconhecer as funções globais no nível acima daqui.
+                    //OBS.: SEM ISSO, não será reconhecida a função this.buscarPartidas().
+                }.bind(this)) 
+
+                //erro
+                .fail(function(erro){
+                    // debugger;
+                })
+
+                ;
+
+            },
+            buscarClassificacao: function () {
+                //obter o model a ser atualizado
+                var classificacaoModel2 = this.getView().getModel("ModeloClassificacao");
+
+                //configuracoes igual do nosso teste no postman
+                // url: preencher com a url da API externa
+                // method: preencher com o tipo do metodo de busca dos dados,
+                // async: preencher verdadeiro ou falso
+                // crossDomain: preencher se a api tem um URL diferente do seu app,
+                // headers: e onde tem as suas autorizacoes de autenticacao da API
+                const configuracoes = {
+                    url: "https://api.api-futebol.com.br/v1/campeonatos/10/tabela",
+                    method: "GET",
+                    async: true,
+                    crossDomain: true,
+                    headers: {
+                        "Authorization": "Bearer live_c01acd31e279debe09670e938d2cb1"
+                    }
+                };
+
+                //fazemos a chamada da API
+                $.ajax(configuracoes)
+                
+                //sucesso
+                .done(function(resposta) {
+                    // debugger;
+                    classificacaoModel2.setData({"Classificacao" : resposta});
+                })
+
+                //erro
+                .fail(function(erro){
+                    // debugger;
+                })
+
+                ;
+
+            },
+            buscarPartidas: function (rodada) {
+                //obter o model a ser atualizado
+                var partidasModel2 = this.getView().getModel("ModeloPartidas");
+
+                //configuracoes igual do nosso teste no postman
+                // url: preencher com a url da API externa
+                // method: preencher com o tipo do metodo de busca dos dados,
+                // async: preencher verdadeiro ou falso
+                // crossDomain: preencher se a api tem um URL diferente do seu app,
+                // headers: e onde tem as suas autorizacoes de autenticacao da API
+                const configuracoes = {
+                    url: "https://api.api-futebol.com.br/v1/campeonatos/10/rodadas/" + rodada,
+                    method: "GET",
+                    async: true,
+                    crossDomain: true,
+                    headers: {
+                        "Authorization": "Bearer live_c01acd31e279debe09670e938d2cb1"
+                    }
+                };
+
+                //fazemos a chamada da API
+                $.ajax(configuracoes)
+                
+                //sucesso
+                .done(function(resposta) {
+                    // debugger;
+                    partidasModel2.setData(resposta);
+                })
+
+                //erro
+                .fail(function(erro){
+                    // debugger;
+                })
+
+                ;
+
             }
         });
     });
